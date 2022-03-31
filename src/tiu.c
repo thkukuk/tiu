17b694429b763072205977f22b132811c4d581ea
Stefan Schubert <schubi@suse.de>
2022-03-31 23:48:37 +0200

    improved error handling

--- src/tiu.c
+++ src/tiu.c
@@ -418,6 +418,8 @@ main(int argc, char **argv)
 	    }
 	  else
 	    g_fprintf (stderr, "ERROR: installation of the archive failed!\n");
+          cleanup_install (bundle);
+	  free_bundle(bundle);
 	  exit (1);
 	}
 
